b94d1b22f7e7bd7f14f7a755a230a8fa2de308ef
FoxyDrew <100545453+FoxyDrew@users.noreply.github.com>
2024-03-12 21:13:36 -0400

    Create variant-script.sh

---
+++ twrp/recovery/root/system/bin/variant-script.sh
@@ -0,0 +1,28 @@
+#!/system/bin/sh
+# This script is needed to automatically set device props.
+
+variant=$(getprop ro.boot.prjname)
+region=$(getprop ro.boot.hw_region_id)
+
+echo $variant
+echo $region
+
+case $region in
+    "21")
+    #NA
+            resetprop ro.product.device "OP5973L1"
+            resetprop ro.product.odm.device "OP5973L1"
+            resetprop ro.product.product.device "OP5973L1"
+            resetprop ro.product.system_ext.device "OP5973L1"
+            resetprop ro.product.vendor.device "OP5973L1"
+            resetprop ro.product.product.model "CPH2451"
+            resetprop ro.product.model "CPH2551"
+            resetprop ro.product.system.model "CPH2551"
+            resetprop ro.product.system_ext.model "CPH2551"
+            resetprop ro.product.vendor.model "CPH2551"
+            resetprop ro.product.odm.model "CPH2551"
+            resetprop ro.boot.hardware.revision "NA"
+        ;;
+esac
+
+exit 0 
